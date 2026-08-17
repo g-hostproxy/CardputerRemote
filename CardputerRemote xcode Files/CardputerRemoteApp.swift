fb650c62651d23713f0e734b2954b7cc7cdeb484
GhostProxy <110871000+g-hostproxy@users.noreply.github.com>
2026-08-17 15:55:45 -0600

    Add files via upload

---
+++ CardputerRemote xcode Files/CardputerRemoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardputerRemoteApp.swift
+//  CardputerRemote
+//
+//  Created by Macbook Pro on 8/14/26.
+//
+
+import SwiftUI
+
+@main
+struct CardputerRemoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
